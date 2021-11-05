e38c42dbeb9bbb2b318912f63b870abffe56f1ed
MuckT <mcgtom10@gmail.com>
2021-11-04 23:50:10 -0700

    chore: fix lint errors semi

--- src/express-api/server.js
+++ src/express-api/server.js
@@ -8,10 +8,10 @@ const socket = io.connect('http://localhost:3000/caps')
 
 const PORT = process.env.PORT || 3001
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.use(express.json())
 
